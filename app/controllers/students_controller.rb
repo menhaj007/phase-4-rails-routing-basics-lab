79514546fd9a7d1c58c6103198a3b97da53a9e37
menhaj <menhaj007@yahoo.com>
2021-07-19 21:41:30 -0400

    commit tree f265b7a1d5444e389faffec646cd7cf402ae4edd

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+    def index
+        render(json: Student.all)
+    end
+    def grades
+        students = Student.order(grade: :desc)
+        render(json: students)
+    end
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render(json: student)
+    end
+end
